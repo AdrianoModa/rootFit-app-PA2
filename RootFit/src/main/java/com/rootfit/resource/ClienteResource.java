81de7793650d0c08fd2b99bfb1d8c0008974c9ad
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-01 12:35:12 -0300

    Inserção @GetMapping e ajuste connector MySQL

--- RootFit/src/main/java/com/rootfit/resource/ClienteResource.java
+++ RootFit/src/main/java/com/rootfit/resource/ClienteResource.java
@@ -1,6 +1,9 @@
 package com.rootfit.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,11 @@ public class ClienteResource {
 	
 	@Autowired
 	private ClienteBO clienteBO;
+	
+	@GetMapping
+	public List<Cliente> listarTodos(){
+		return clienteBO.ListarTodosClientes();
+	}
 	
 	@PostMapping("/adicionar/{nomeAluno}/{peso}/{altura}/{matricula}")
 	public Cliente adicionar(
