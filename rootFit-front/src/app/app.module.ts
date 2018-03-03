4f54217d21a56c262527f032a16183f570c4a2af
Maikon Albuquerque <maikonmorais@gmail.com>
2018-03-03 14:22:50 -0300

    adicionando componente listagem de clientes

--- rootFit-front/src/app/app.module.ts
+++ rootFit-front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListarClientesComponent } from './listar-clientes/listar-clientes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarClientesComponent
   ],
   imports: [
     BrowserModule
