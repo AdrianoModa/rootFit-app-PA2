510be2ac3c9d09ed071df4210c84d8e6f8ea567f
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-06 00:07:59 -0300

    Cross Origin setado

--- RootFit/src/main/java/com/rootfit/resource/ClienteResource.java
+++ RootFit/src/main/java/com/rootfit/resource/ClienteResource.java
@@ -3,6 +3,7 @@ package com.rootfit.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.rootfit.model.Cliente;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin("${origem-permitida}")
 public class ClienteResource {
 	
 	@Autowired
