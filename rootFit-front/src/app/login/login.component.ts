39382cc1acc205afb6f1199181e8390b97e0b0f9
Maikon Albuquerque <maikonmorais@gmail.com>
2018-03-08 13:24:00 -0300

    inserindo requisição http no component login

--- rootFit-front/src/app/login/login.component.ts
+++ rootFit-front/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { nullSafeIsEquivalent } from '@angular/compiler/src/output/output_ast';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-login',
@@ -11,20 +13,20 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor( private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
-
-      // this.loginForm = new FormGroup({
-      //   email: new FormControl(null),
-      //   senha: new FormControl(null)
-      // });
-
       this.loginForm = this.formBuilder.group({
         email: [null],
         senha: [null]
       });
+  }
+  onSubmit() {
+    console.log(this.loginForm.value);
 
+    this.http.post('http://localhost:8080/aluno', JSON.stringify(this.loginForm.value))
+    .map(res => res)
+    .subscribe(dados => console.log(dados));
   }
 
 }
--- rootFit-front/src/app/login/login.module.ts
+++ rootFit-front/src/app/login/login.module.ts
@@ -1,13 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SharedModule } from './../shared/shared.module';
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [
    LoginComponent
