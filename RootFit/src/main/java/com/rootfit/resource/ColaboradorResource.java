860caef62e85edca1ffd533a2834bc37fbddfbb3
Maikon Albuquerque <maikonmorais@gmail.com>
2018-03-10 16:30:54 -0300

    Inserção do método DELETE no back

--- RootFit/src/main/java/com/rootfit/resource/ColaboradorResource.java
+++ RootFit/src/main/java/com/rootfit/resource/ColaboradorResource.java
@@ -6,7 +6,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +32,10 @@ public class ColaboradorResource {
 	public Colaborador adicionar(@RequestBody @Valid Colaborador colaborador) {
 		return colaboradorDAO.save(colaborador);
 		
+	}
+	@DeleteMapping("/colaborador/{id}")
+	public void remover(@PathVariable Long id) {
+		colaboradorDAO.delete(id);
 	}
 	
 	
