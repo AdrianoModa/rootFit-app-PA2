b2974d5ab4541d1a7a770b42f6c235d0f1fc1074
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-25 18:57:23 -0300

    Correcao Classe na pasta errada

--- RootFit/src/main/java/com/rootfit/services/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rootfit.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rootfit.model.Estado;
-
-public interface EstadoRepository extends JpaRepository<Estado, Long> {
-
-}
