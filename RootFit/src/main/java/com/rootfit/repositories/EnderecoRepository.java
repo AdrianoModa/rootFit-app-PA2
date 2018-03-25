1bbf644bcd33b3d5f22db62e4f5b7f04cbb5fe87
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-25 18:16:51 -0300

    Repositorio da entidade Endereco criado.

---
+++ RootFit/src/main/java/com/rootfit/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.rootfit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootfit.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
