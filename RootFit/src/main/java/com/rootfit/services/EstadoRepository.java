e1d74f14b44421005fe4b0cd2f69c2db2d70346d
adrianomoda <adrianomodafeitosa@gmail.com>
2018-03-25 18:52:09 -0300

    Repositorio entidade Estado

---
+++ RootFit/src/main/java/com/rootfit/services/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.rootfit.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootfit.model.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
