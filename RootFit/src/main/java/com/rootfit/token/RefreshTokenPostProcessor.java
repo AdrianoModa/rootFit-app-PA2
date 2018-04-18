1e58c375548f999b5a7811d3df43439aa11ed168
Maikon Albuquerque <maikonmorais@gmail.com>
2018-04-18 00:23:15 -0300

    Cookie de segurança adicionado ao serviço de login

--- RootFit/src/main/java/com/rootfit/token/RefreshTokenPostProcessor.java
+++ RootFit/src/main/java/com/rootfit/token/RefreshTokenPostProcessor.java
@@ -1,5 +1,56 @@
 package com.rootfit.token;
 
-public class RefreshTokenPostProcessor {
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> aClass) {
+        return returnType.getMethod().getName().equals("postAccessToken");
+    }
+
+    @Override
+    public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType,
+                                             MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass,
+                                             ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+        HttpServletRequest req = ((ServletServerHttpRequest)serverHttpRequest).getServletRequest();
+        HttpServletResponse resp = ((ServletServerHttpResponse)serverHttpResponse).getServletResponse();
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+
+        String refreshToken = body.getRefreshToken().getValue();
+        addRefreshTokenCookie(refreshToken, req, resp);
+        removeRefreshTokenBody(token);
+
+        return body;
+    }
+
+    private void removeRefreshTokenBody(DefaultOAuth2AccessToken token) {
+        token.setRefreshToken(null);
+    }
+
+    private void addRefreshTokenCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false); //TODO: Mudar para true em produção
+        refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+        refreshTokenCookie.setMaxAge(2592000);
+        resp.addCookie(refreshTokenCookie);
+    }
 }
