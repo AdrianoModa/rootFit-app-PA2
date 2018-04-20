07d0b7e7aeba645b4477bedbe1e96d8e40d58060
root <root@alba.alba>
2018-04-20 17:07:12 -0300

    login funcionando com dados persistidos

---
+++ RootFit/src/main/java/com/rootfit/security/util/GeradorSenha.java
@@ -0,0 +1,12 @@
+package com.rootfit.security.util;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+
+    public static void main(String[] args){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin"));
+    }
+}
