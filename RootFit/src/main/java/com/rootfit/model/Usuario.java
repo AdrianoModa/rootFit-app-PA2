d445338140610ae2297aa0b89fab5a145cc5486c
matheustanaques <matheustanaques@gmail.com>
2018-04-25 22:35:11 -0300

    model instrutor criado

---
+++ RootFit/src/main/java/com/rootfit/model/Usuario.java
@@ -0,0 +1,161 @@
+package com.rootfit.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String cpf;
+	private String email;
+	private String genero;
+	private String login;
+	private String matricula;
+	private String nome;
+	private String senha;
+	private String rg;
+	private String telefone;
+	
+	private List<Endereco> enderecos = new ArrayList();
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(String id, String cpf, String email, String genero, String login, String matricula, String nome,
+			String senha, String rg, String telefone) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.email = email;
+		this.genero = genero;
+		this.login = login;
+		this.matricula = matricula;
+		this.nome = nome;
+		this.senha = senha;
+		this.rg = rg;
+		this.telefone = telefone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
