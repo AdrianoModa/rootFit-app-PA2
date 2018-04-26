365070e5303b2fc8d37e4922d6a2549030f697f8
matheustanaques <matheustanaques@gmail.com>
2018-04-26 18:28:39 -0300

    refatorado o model aluno

--- RootFit/src/main/java/com/rootfit/model/Aluno.java
+++ RootFit/src/main/java/com/rootfit/model/Aluno.java
@@ -2,61 +2,70 @@ package com.rootfit.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-@Table
-public class Aluno implements Serializable {
+public class Aluno extends Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
-	private String nomeAluno;
-	private String email;
-	private String senha;
-	private float peso;
-	private float altura;
-	private String matricula;
-
-	private Endereco endereco;
+	private Boolean status;
+	private String objetivo;
+	private Date nascimento;
+	private Date inicio;
 	
-	@ManyToOne
-	@JoinColumn(name="instrutor_id")
 	private Instrutor instrutor;
-
-	@OneToMany(mappedBy="aluno")
-	private List<AvaliacaoFisica> avaliacoes = new ArrayList<AvaliacaoFisica>();
 	
-	@OneToOne(cascade=CascadeType.ALL, mappedBy="aluno")
-	private FichaTreino fichaTreino;
-
-	/* Getters e Setters */
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	public Long getId() {
-		return id;
+	private List<AvaliacaoFisica> avaliacoesFisicas = new ArrayList<>();
+	private List<FichaTreino> fichasTreino = new ArrayList<>();
+	
+	public Aluno() {
+		
+	}
+	
+	public Aluno(Boolean status, String objetivo, Date nascimento, Date inicio, Instrutor instrutor) {
+		super();
+		this.status = status;
+		this.objetivo = objetivo;
+		this.nascimento = nascimento;
+		this.inicio = inicio;
+		this.instrutor = instrutor;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public Boolean getStatus() {
+		return status;
 	}
 
-	@ManyToOne
-	@JoinColumn(name = "instrutor_id")
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public String getObjetivo() {
+		return objetivo;
+	}
+
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public Date getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+
 	public Instrutor getInstrutor() {
 		return instrutor;
 	}
@@ -65,99 +74,24 @@ public class Aluno implements Serializable {
 		this.instrutor = instrutor;
 	}
 
-	@ManyToOne
-	@JoinColumn(name = "endereco_id")
-	public Endereco getEndereco() {
-		return endereco;
+	public List<AvaliacaoFisica> getAvaliacoesFisicas() {
+		return avaliacoesFisicas;
 	}
 
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
+	public void setAvaliacoesFisicas(List<AvaliacaoFisica> avaliacoesFisicas) {
+		this.avaliacoesFisicas = avaliacoesFisicas;
 	}
 
-
-	@NotEmpty
-	@Column
-	public String getNomeAluno() {
-		return nomeAluno;
+	public List<FichaTreino> getFichasTreino() {
+		return fichasTreino;
 	}
 
-	public void setNomeAluno(String nomeAluno) {
-		this.nomeAluno = nomeAluno;
+	public void setFichasTreino(List<FichaTreino> fichasTreino) {
+		this.fichasTreino = fichasTreino;
 	}
-
-	@NotEmpty
-	@Column
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@NotEmpty
-	@Column
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@NotEmpty
-	@Column
-	public float getPeso() {
-		return peso;
-	}
-
-	public void setPeso(float peso) {
-		this.peso = peso;
-	}
-
-	@NotEmpty
-	@Column
-	public float getAltura() {
-		return altura;
-	}
-
-	public void setAltura(float altura) {
-		this.altura = altura;
-	}
-
-	@NotEmpty
-	@Column
-	public String getMatricula() {
-		return matricula;
-	}
-
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Aluno other = (Aluno) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+	
+	
+	
+	
+	
 }
