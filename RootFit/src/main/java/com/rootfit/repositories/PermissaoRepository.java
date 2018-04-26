b17e14014dd74149fdd218ccf3c729c476fd8c99
adrianomoda <adrianomodafeitosa@gmail.com>
2018-04-26 10:16:30 -0300

    Repositorio Permissao

---
+++ RootFit/src/main/java/com/rootfit/repositories/PermissaoRepository.java
@@ -0,0 +1,9 @@
+package com.rootfit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootfit.model.Permissao;
+
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+
+}
