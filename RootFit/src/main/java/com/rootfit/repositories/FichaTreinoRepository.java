04dafb22e230caab3ae818abcc5e4dca8779b985
adrianomoda <adrianomodafeitosa@gmail.com>
2018-04-26 21:18:30 -0300

    Repositorio FichaTreino

---
+++ RootFit/src/main/java/com/rootfit/repositories/FichaTreinoRepository.java
@@ -0,0 +1,9 @@
+package com.rootfit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootfit.model.FichaTreino;
+
+public interface FichaTreinoRepository extends JpaRepository<FichaTreino, Long> {
+
+}
