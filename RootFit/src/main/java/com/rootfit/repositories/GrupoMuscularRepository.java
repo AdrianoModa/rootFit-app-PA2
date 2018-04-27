b80f98bf808066f253e33a32928501b8900ae90a
adrianomoda <adrianomodafeitosa@gmail.com>
2018-04-26 21:21:57 -0300

    Repositorio Grupo Muscular

---
+++ RootFit/src/main/java/com/rootfit/repositories/GrupoMuscularRepository.java
@@ -0,0 +1,9 @@
+package com.rootfit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rootfit.model.GrupoMuscular;
+
+public interface GrupoMuscularRepository extends JpaRepository<GrupoMuscular, Long> {
+
+}
